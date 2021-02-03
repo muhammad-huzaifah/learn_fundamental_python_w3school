218b1ac3f50edd5db25f216af356079d63b6548b
muhammad-huzaifah <huzaifah2009@gmail.com>
2021-02-03 13:41:57 +0700

    new file

---
+++ algoritms.py
@@ -0,0 +1,11 @@
+jam_lembur = 12
+upah_kerja = 200000
+
+if jam_lembur < upah_kerja:
+    print("gaji lembur sebesar Rp. 100.000")
+if jam_lembur > upah_kerja:
+    print("gaji lembur sebesar Rp. 300.000")
+if jam_lembur == upah_kerja:
+    print("gaji lembur sebesar Rp. 200.000")
+else:
+    print("tidak ada upah_lembur")
