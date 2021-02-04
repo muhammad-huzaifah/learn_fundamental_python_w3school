0c9e773d8134c6692823af5e93f1c264951d8149
Muhammad Huzaifah <huzaifah2009@gmail.com>
2021-02-04 17:33:54 +0700

    add join list

--- learn_python.py
+++ learn_python.py
@@ -866,3 +866,41 @@ def myfunc(n):
 thislist = [100, 50, 65, 82, 23]
 thislist.sort(key=myfunc)
 print(thislist)
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort()
+print(thislist)
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key=str.lower)
+print(thislist)
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.reverse()
+print(thislist)
+
+# copy list
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+
+# join list
+
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
+
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+    list1.append(x)
+
+print(list1)
