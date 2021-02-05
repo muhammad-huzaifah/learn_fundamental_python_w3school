eb753a15fcedf128b9c5e23dba2a181cefbbe157
muhammad-huzaifah <huzaifah2009@gmail.com>
2021-02-05 16:45:35 +0700

    add code comments, variables, data types, numbers, casting, string, boolean, operator, list and tuples

--- learn_python.py
+++ learn_python.py
@@ -904,3 +904,162 @@ for x in list2:
     list1.append(x)
 
 print(list1)
+
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1)
+
+# list method
+a = ["apple", "banana", "cherry"]
+b = ["ford", "BMW", "Volvo"]
+a.append(b)
+print(a)
+
+fruits = ["apple", "banana", "cherry", ]
+fruits.clear()
+print(fruits)
+
+# tuples
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+thistuple = ("apple",)
+print(type(thistuple))
+
+# not tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+print(tuple1)
+print(tuple2)
+print(tuple3)
+
+tuple1 = ("abc", 34, True, 40, "male")
+print(tuple1)
+
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))
+
+thistuple = tuple(("apple", "banana", "cherry"))
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+    print("Yes, 'apple' is in the fruits tuple")
+
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
+
+# This will raise an error
+thistuple = ("apple", "banana", "cherry")
+# thistuple.append("orange")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+print(thistuple)
+
+# this will raise an error because the tuple no longer exists
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+# print(thistuple)
+
+fruits = ("apple", "banana", "cherry")
+
+(green, yellow, red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+fruits = ("apple", "banana", "cherry", "strawberry", "rasberry")
+
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+fruits = ("apple", "manggo", "papaya", "pineapple", "cherry")
+
+(green, *tropic, red) = fruits
+
+print(green)
+print(tropic)
+print(red)
+
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+    print(x)
+
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+    print(thistuple[i])
+
+thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+    print(thistuple[i])
+    i = i + 1
+
+tuple1 = ("a", "b", "c")
+tuple2 = (1, 2, 3)
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+print(mytuple)
+
+thistuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+
+x = thistuple.count(5)
+
+print(x)
+
+thistuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+x = thistuple.index(8)
+print(x)
