1e7d5b24d57eaad53452b434cc09eb1757a1fd78
muhammad-huzaifah <huzaifah2009@gmail.com>
2021-02-10 16:22:37 +0700

    add file for_loops : learn code for loops

---
+++ for_loops.py
@@ -0,0 +1,80 @@
+#for loops
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+
+#looping through a string
+
+for x in "banana":
+    print(x)
+
+#break statement
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
+
+#continue statement
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        continue
+    print(x)
+
+#range function
+for x in range(6):
+    print(x)
+
+for x in range(2, 6):
+    print(x)
+
+for x in range(2, 30, 3):
+    print(x)
+
+#else for loop
+for x in range(6):
+    print(x)
+else:
+    print("Finally finished")
+
+for x in range(6):
+    if x == 3: break
+    print(x)
+else:
+    print("Finally finished")
+
+#need loop
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+    for y in fruits:
+        print(x, y)
+
+for x in [0, 1, 2]:
+    pass
+
+#exercise
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        continue
+    print(x)
+
+for x in range(6):
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        break
+    print(x)
