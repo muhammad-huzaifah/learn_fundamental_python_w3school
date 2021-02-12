34e197f3961072672add8c486b3ebcbe489a7d9f
muhammad-huzaifah <huzaifah2009@gmail.com>
2021-02-12 15:21:00 +0700

    add file function : learn code array

---
+++ array.py
@@ -0,0 +1,37 @@
+# array
+cars = ["Ford", "Volvo", "BMW"]
+print(cars)
+
+# Access elemen array
+cars = ["Ford", "Volvo", "BMW"]
+
+x = cars[0]
+print(x)
+
+cars = ["Ford", "Volvo", "BMW"]
+cars[0] = "Toyota"
+print(cars)
+
+# len array
+cars = ["Ford", "Volvo", "BMW"]
+x = len(cars)
+print(x)
+
+# looping array elements
+cars = ["Ford", "Volvo", "BMW"]
+for x in cars:
+    print(x)
+
+# adding array elements
+cars = ["Ford", "Volvo", "BMW" ]
+cars.append("Honda")
+print(cars)
+
+# removing array elements
+cars = ["Ford", "Volvo", "BMW" ]
+cars.pop(1)
+print(cars)
+
+cars = ["Ford", "Volvo", "BMW" ]
+cars.remove("Volvo")
+print(cars)
