50d112c6c5c19cdca7fd73f68872d454f36a58d0
muhammad-huzaifah <huzaifah2009@gmail.com>
2021-02-14 17:32:24 +0700

    add file function : learn code iterator

---
+++ iterator.py
@@ -0,0 +1,72 @@
+# iterator
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+mystr = "banana"
+myit = iter(mystr)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+# loopiing iterator
+mytuple = ("apple", "banana", "cherrry")
+
+for x in mytuple:
+    print(x)
+
+mystr = "banana"
+
+for x in mystr:
+    print(x)
+
+
+# create iterator
+class MyNumber:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
+
+
+myclass = MyNumber()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+
+
+# stop iterator
+class MyNumber:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        if self.a <= 20:
+            x = self.a
+            self.a += 1
+            return x
+        else:
+            raise StopIteration
+
+
+myclass = MyNumber()
+myiter = iter(myclass)
+
+for x in myiter:
+    print(x)
