3908b28a823602c5be4c9fb96fbcd4c7dadbab78
muhammad-huzaifah <huzaifah2009@gmail.com>
2021-02-16 17:20:43 +0700

    add file date : learn code date

--- date.py
+++ date.py
@@ -4,3 +4,17 @@ import datetime
 x = datetime.datetime.now()
 
 print(x)
+
+# date output
+print(x.year)
+print(x.strftime("%A"))
+
+
+# Create Date Objects
+x = datetime.datetime(2020, 5, 17)
+
+print(x)
+
+# strftime() methode
+x = datetime.datetime(2018, 6, 1)
+print(x.strftime("%B"))
