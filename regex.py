dd3c1471c2a21f11696b5e13ebed7f00f7da5a93
muhammad-huzaifah <huzaifah2009@gmail.com>
2021-02-18 19:43:02 +0700

    add file regex : learn regex

---
+++ regex.py
@@ -0,0 +1,52 @@
+# regex
+import re
+
+txt = "The rain in Spain"
+x = re.search("^The.*Spain$", txt)
+if x:
+    print("YES! We have a match!")
+else:
+    print("No match")
+
+# function
+txt = "The rain in Spain"
+x = re.findall("ai", txt)
+print(x)
+
+txt = "The rain in Spain"
+x = re.findall("Portugal", txt)
+print(x)
+
+x = re.search('\s', txt)
+print("The first white-space character is located in position:", x.start())
+
+x = re.search("Portugal", txt)
+print(x)
+
+txt = "The rain in Spain"
+x = re.split("\s", txt)
+print(x)
+
+txt = "The rain in Spain"
+x = re.split("\s", txt, 1)
+print(x)
+
+txt = "The rain in Spain"
+x = re.sub("\s", "9", txt)
+print(x)
+
+txt = "The rain in Spain"
+x = re.sub("\s", "9", txt, 2)
+print(x)
+
+x = re.search("ai", txt)
+print(x)
+
+x = re.search(r"\bS\w+", txt)
+print(x.span())
+
+x = re.search(r"\bS\w+", txt)
+print(x.string)
+
+x = re.search(r"\bS\w+", txt)
+print(x.group())
