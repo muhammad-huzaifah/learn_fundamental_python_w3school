1119ca9981a6719b52a3533afb053a20495e90f7
muhammad-huzaifah <huzaifah2009@gmail.com>
2021-02-20 08:15:03 +0700

    add file read_file.py for learn read file python

--- read_file.py
+++ read_file.py
@@ -1,2 +1,27 @@
 f = open("demofile.txt", "r")
 print(f.read())
+
+f = open("c:\\myfiles\welcome.txt", "r")
+print(f.read())
+
+# read only parts of the file
+f = open("demofile.txt", "r")
+print(f.read(5))
+
+# read lines
+f = open("demofile.txt", "r")
+print(f.readline())
+
+# 2 read lines
+f = open("demofile.txt", "r")
+print(f.readline())
+print(f.readline())
+
+f = open("demofile.txt", "r")
+for x in f:
+    print(x)
+
+# close file
+f = open("demofile.txt", "r")
+print(f.readline())
+f.close()
